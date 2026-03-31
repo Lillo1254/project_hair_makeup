9d1f411264c52448fc530dd868bc05fac9799efc
alessandroFix <alessandrolucia75@gmail.com>
2026-03-31 18:34:11 +0200

    change sitemap whit priority and lastModified

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,18 +2,18 @@ export default function sitemap() {
   const baseUrl = "https://atestainsu.vercel.app";
 
   return [
-    { url: `${baseUrl}/` },
-    { url: `${baseUrl}/chi-siamo` },
-    { url: `${baseUrl}/contatti` },
-    { url: `${baseUrl}/servicesPage` },
-    { url: `${baseUrl}/galleryPage` },
-    { url: `${baseUrl}/prenotazioni` },
-    { url: `${baseUrl}/privacy-policy` },
+    { url: `${baseUrl}/`, priority: 1 , lastModified: new Date() },
+    { url: `${baseUrl}/chi-siamo`, priority: 0.7 , lastModified: new Date() },
+    { url: `${baseUrl}/contatti`, priority: 0.8 , lastModified: new Date() },
+    { url: `${baseUrl}/servicesPage`, priority: 0.8 , lastModified: new Date() },
+    { url: `${baseUrl}/galleryPage`, priority: 0.5 , lastModified: new Date() },
+    { url: `${baseUrl}/prenotazioni`, priority: 0.9 , lastModified: new Date() },
+    { url: `${baseUrl}/privacy-policy`, priority: 0.2 , lastModified: new Date() },
 
-    // dinamiche
-    { url: `${baseUrl}/servicesPage/uomo` },
-    { url: `${baseUrl}/servicesPage/donna` },
-    { url: `${baseUrl}/servicesPage/bambino` },
-    { url: `${baseUrl}/servicesPage/make-up` },
+    
+    { url: `${baseUrl}/servicesPage/uomo`, priority: 0.6 , lastModified: new Date() },
+    { url: `${baseUrl}/servicesPage/donna`, priority: 0.8 , lastModified: new Date() },
+    { url: `${baseUrl}/servicesPage/bambino`, priority: 0.7 , lastModified: new Date() },
+    { url: `${baseUrl}/servicesPage/make-up`, priority: 0.6 , lastModified: new Date() },
   ];
 }
