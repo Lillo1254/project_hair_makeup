738d8550b21d7ba2081e8fb963286645f7c14961
Lillo1254 <alessandrolucia75@gmail.com>
2026-04-08 02:59:39 +0200

    Update CardGallery.jsx height

--- app/components/cards/CardGallery.jsx
+++ app/components/cards/CardGallery.jsx
@@ -4,7 +4,7 @@ return (
 <img
 src={image}
 alt="parrucchiere guidonia roma"
-className="h-40 w-full object-cover grayscale-55 hover:grayscale-0 md:grayscale md:hover:grayscale-0 transition rounded-md"
+className="h-40 md:h-96 w-full object-cover grayscale-55 hover:grayscale-0 md:grayscale md:hover:grayscale-0 transition rounded-md"
 />
 );
 }
